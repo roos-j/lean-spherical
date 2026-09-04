d45bcdce0a6b68a3fea0581cba6dd84fdce31186
Joris Roos <jroos.math@gmail.com>
2026-09-03 23:10:13 -0400

    update proofs

--- LeanSpherical/Theorems.lean
+++ LeanSpherical/Theorems.lean
@@ -17,26 +17,26 @@ open scoped Spherical ENNReal NNReal Topology SchwartzMap
 theorem eLpNorm_sphericalMaximal_le_schwartzMap {d : ℕ} {p : ℝ≥0∞} (hd : 2 ≤ d)
     (hp : (d : ℝ≥0∞) / (d - 1) < p) :
     ∃ C : ℝ, ∀ f : 𝓢(ℝ^d, ℂ),
-      eLpNorm (M (Ioi 0) f) p volume ≤ (ENNReal.ofReal C) * eLpNorm f p volume := by
-  sorry
+      eLpNorm (M (Ioi 0) f) p volume ≤ (ENNReal.ofReal C) * eLpNorm f p volume :=
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_sphericalMaximal_le_schwartzMap hd hp
 
 /-- Spherical maximal theorem (`d = 2`: Bourgain, `d ≥ 3`: Stein), `L^p` version -/
 theorem eLpNorm_sphericalMaximal_le {d : ℕ} {p : ℝ≥0∞} (hd : 2 ≤ d)
     (hp : (d : ℝ≥0∞) / (d - 1) < p) :
     ∃ C : ℝ, ∀ f : (ℝ^d) → ℂ, MemLp f p volume →
-      ∀ᵐ x ∂volume, ∀ t ∈ Ioi 0,
+      ∀ᵐ x ∂volume, ∀ t ∈ Ioi (0 : ℝ),
         Integrable (fun y : unitSphere d ↦ f (x + t • (y : ℝ^d))) (unitSphereMeasure d) ∧
       MemLp (M (Ioi 0) f) p volume ∧
       eLpNorm (M (Ioi 0) f) p volume ≤ (ENNReal.ofReal C) * eLpNorm f p volume :=
-  sorry
-  -- Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_sphericalMaximal_le hd hp
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_sphericalMaximal_le_of_memLp hd hp
 
 /-- Sharpness of Bourgain's and Stein's theorems -/
 theorem eLpNorm_sphericalMaximal_eq_top_of_le_criticalExponent {d : ℕ} {p : ℝ≥0∞} (hd : 2 ≤ d)
     (hp0 : 0 < p) (hp : p ≤ (d : ℝ≥0∞) / (d - 1)) :
     ∃ f : 𝓢(ℝ^d, ℂ), 0 < eLpNorm f p volume ∧
-      eLpNorm (M (Ioi 0) f) p volume = ⊤ := by
-  sorry
+      eLpNorm (M (Ioi 0) f) p volume = ⊤ :=
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_sphericalMaximal_eq_top_schwartzMap
+    hd hp0 hp
 
 namespace RestrictedDilations
 
@@ -45,8 +45,9 @@ theorem eLpNorm_restrictedSphericalMaximal_le_schwartzMap {d : ℕ} {p : ℝ≥0
     (hd : 2 ≤ d) {E : Set ℝ} (hE : E ⊆ Ioi 0)
     (hp : ENNReal.ofReal (criticalExponent d E) < p) :
     ∃ C : ℝ, ∀ f : 𝓢(ℝ^d, ℂ),
-      eLpNorm (M E f) p volume ≤ (ENNReal.ofReal C) * eLpNorm f p volume := by
-  sorry
+      eLpNorm (M E f) p volume ≤ (ENNReal.ofReal C) * eLpNorm f p volume :=
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_restrictedSphericalMaximal_le_schwartzMap
+    hd hE hp
 
 /-- Seeger-Wainger-Wright theorem, `L^p` version -/
 theorem eLpNorm_restrictedSphericalMaximal_le {d : ℕ} {p : ℝ≥0∞}
@@ -57,9 +58,8 @@ theorem eLpNorm_restrictedSphericalMaximal_le {d : ℕ} {p : ℝ≥0∞}
         Integrable (fun y : unitSphere d ↦ f (x + t • (y : ℝ^d))) (unitSphereMeasure d) ∧
       MemLp (M E f) p volume ∧
       eLpNorm (M E f) p volume ≤ (ENNReal.ofReal C) * eLpNorm f p volume :=
-  sorry
-  -- Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_restrictedSphericalMaximal_le
-  --   hd hE hp
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_restrictedSphericalMaximal_le_of_memLp
+    hd hE hp
 
 /-- Sharpness up to endpoints of Seeger-Wainger-Wright theorem -/
 theorem eLpNorm_restrictedSphericalMaximal_ge_of_lt_criticalExponent {d : ℕ} {p : ℝ≥0∞}
@@ -67,17 +67,15 @@ theorem eLpNorm_restrictedSphericalMaximal_ge_of_lt_criticalExponent {d : ℕ} {
     (hp0 : 0 < p) (hp : p < ENNReal.ofReal (criticalExponent d E)) :
     ∀ C : ℝ, ∃ f : 𝓢(ℝ^d, ℂ),
       eLpNorm (M E f) p volume ≥ (ENNReal.ofReal C) * eLpNorm f p volume :=
-  sorry
-  -- Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_restrictedSphericalMaximal_ge_of_lt_criticalExponent
-  --   hd hEne hE hp0 hp
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_restricted_ge_schwartz hd hEne hE hp0 hp
 
 /-- C.P. Calderon's theorem -/
 theorem eLpNorm_lacunarySphericalMaximal_le_schwartzMap {d : ℕ} {p : ℝ≥0∞} (hd : 2 ≤ d)
     (hp : 1 < p) :
     ∃ C : ℝ, ∀ f : 𝓢(ℝ^d, ℂ),
       eLpNorm (M {2 ^ k | k : ℤ} f) p volume ≤ (ENNReal.ofReal C) * eLpNorm f p volume :=
-  sorry
-  -- Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_lacunarySphericalMaximal_le hd hp
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.eLpNorm_lacunarySphericalMaximal_le_schwartzMap
+    hd hp
 
 end RestrictedDilations
 
@@ -87,8 +85,7 @@ namespace PowerWeights
 theorem closure_typeSet_eq
     {d : ℕ} (hd : 2 ≤ d) {E : Set ℝ} (hE : E.Nonempty) (hEpos : E ⊆ Ioi 0) :
     closure (typeSet d E) = admissibleRegion d E :=
-  sorry
-  -- Auto.Spherical.FractalDilations.AHRSUpperBounds.closure_typeSet_eq hd hE hEpos
+  Auto.Spherical.FractalDilations.AHRSUpperBounds.closure_typeSet_eq hd hE hEpos
 
 end PowerWeights
 
